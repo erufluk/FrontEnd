56c7c44538c0ea3c42e5b1bdb53dd51c4cc71c94
erufluk <erufluk@gmail.com>
2021-02-24 19:42:49 -0300

    Correção de conflitos no Git

--- Projeto  FrontEnd/Angular/src/app/app.module.ts
+++ Projeto  FrontEnd/Angular/src/app/app.module.ts
@@ -5,16 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
-import { EntrarComponent } from './entrar/entrar.component';
-import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { OngComponent } from './ong/ong.component';
+import { ApoiadorComponent } from './apoiador/apoiador.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     RodapeComponent,
-    EntrarComponent,
-    CadastrarComponent
+    OngComponent,
+    ApoiadorComponent
   ],
   imports: [
     BrowserModule,
--- Projeto  FrontEnd/Angular/src/app/cadastrar/cadastrar.component.ts
+++ Projeto  FrontEnd/Angular/src/app/cadastrar/cadastrar.component.ts
@@ -1,15 +1,44 @@
+
+import { AuthService } from './../service/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from '../model/Usuario';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-cadastrar',
   templateUrl: './cadastrar.component.html',
   styleUrls: ['./cadastrar.component.css']
 })
+
 export class CadastrarComponent implements OnInit {
 
-  constructor() { }
+  usuario: Usuario = new Usuario
+  confirmarSenha: string
+  tipoUsuario: string
 
-  ngOnInit(): void {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    
+  ) { }
+
+  ngOnInit() {
+    window.scroll(0,0)
   }
+
+  confirmSenha(event: any) {
+    this.confirmarSenha = event.target.value
+  }
+
+  tipoUser(event: any){
+    this.tipoUsuario = event.target.value
+  }
+
+  cadastrar(event: any){
+    this.usuario.tipo = event.target.value
+
+  }
+
 
 }
